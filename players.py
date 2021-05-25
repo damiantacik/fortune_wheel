4580a6776fbfa511288e1e3494eb9a0f10b70eeb
damtac <damiantacik@gmail.com>
2021-05-25 15:37:12 +0000

    add functionality to change player to next one and get winner

--- players.py
+++ players.py
@@ -31,11 +31,44 @@ def update_score(amount):
     else:
         active_player["score"] = 0
 
+
+def next_player():
+    active_player = get_active_player()
+    active_player_idx = players.index(active_player)
+
+    next_player_idx = (active_player_idx + 1) % len(players)
+
+    active_player["is_active"] = False
+    players[next_player_idx]["is_active"] = True
+
+
+def get_winner():
+    high_score = max([player["score"] for player in players])
+
+    return [player for player in players if player["score"] == high_score]
+
+update_score(600)
+next_player()
+update_score(500)
+next_player()
+update_score(20)
+print(get_winner())
+
+next_player()
+print(players)
+next_player()
+print(players)
+next_player()
+print(players)
+next_player()
+print(players)
+
 update_score(400)
 update_score(200)
 print(players)
 update_score(0)
 print(players)
 
+print(get_active_player())  
 
 
