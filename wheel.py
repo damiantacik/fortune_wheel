1ee1ec41777259209fd339d75f243da8d51ee817
damtac <damiantacik@gmail.com>
2021-05-25 17:18:11 +0000

    spin wheel returns item from wheel

--- wheel.py
+++ wheel.py
@@ -1,10 +1,10 @@
 from random import randint
 
-wheel =[200, 300, 500, 1000, 100, 50, 0, -1, 200, 300, 500, 1000, 100, 50, 200, 300, 500, 1000, 100, 50]
+wheel = [200, 300, 500, 1000, 100, 50, 0, -1, 200, 300, 500, 1000, 100, 50, 200, 300, 500, 1000, 100, 50]
 
 
 def spin_wheel():
-    return wheel[randint(0, len(wheel))]
+    return wheel[randint(0, len(wheel) - 1)]
 
 
 print(spin_wheel())
